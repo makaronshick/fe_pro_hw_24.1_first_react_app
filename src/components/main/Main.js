f7ad99d492b26274931866bc8ce9f9475cab7b55
makaronshick <makaronshick@ukr.net>
2024-10-01 18:48:26 +0300

    fix

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -3,16 +3,18 @@ import LightButton from "../buttons/LightButton.js";
 import Input from "../input/Input.js";
 import Response from "../response/Response.js";
 
-import "./main.div.styles.css"
+import "./main.div.styles.css";
 
 export default () => {
-    return <>
-    <div className="main_div">
-    <Input link="https://swapi.dev/api/"/>
-    <LightButton text="Get info"/>
-  </div>
-  <div>
-  <Response/>
-  </div>
-    </>
-}
+  return (
+    <React.Fragment>
+      <div className="main_div">
+        <Input link="https://swapi.dev/api/" />
+        <LightButton text="Get info" />
+      </div>
+      <div>
+        <Response />
+      </div>
+    </React.Fragment>
+  );
+};
